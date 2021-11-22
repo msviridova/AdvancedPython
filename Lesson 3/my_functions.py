aa7ad175abcbaf15c2b936e59898f7231882ad39
Mari Sviridova <84466460+msviridova@users.noreply.github.com>
2021-11-22 17:46:24 +0300

    Revert "Вроде теперь все добавилось"

--- Lesson 3/my_functions.py
+++ Lesson 3/my_functions.py
@@ -1,9 +1,5 @@
-def fibonacci_func(num: int):
-    if not isinstance(num, int):
-        raise TypeError
-    fib_row = [num, (num - 1) + num]
-    if num in (0, 1):
-        fib_row = [0, 1]
+def fibonacci(num):
+    fib_row = [num, (num-1) + num]
     while len(fib_row) < 10:
         fib_num_1 = fib_row[-1]
         fib_num_2 = fib_row[-2]
@@ -12,10 +8,7 @@ def fibonacci_func(num: int):
     return list(fib_row)
 
 
-print(fibonacci_func(0))
-# print(fibonacci_func(5))
-# print(fibonacci_func(10))
-# print(fibonacci_func("str"))
+print(fibonacci(1))
 
 
 def max_two(a, b, c):
@@ -23,8 +16,5 @@ def max_two(a, b, c):
 
 
 print(max_two(2, 4, 9))
-print(max_two(7, 12, 25))
-print(max_two(45, 289, 400))
-print(max_two(0, 0, 1))
 
 
